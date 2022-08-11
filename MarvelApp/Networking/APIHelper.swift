663915e95ebdca183896ae5e6dcf43c5546a2098
Randy Efan Jayaputra <randyrandyrej8@gmail.com>
2022-08-11 22:36:13 +0700

    add api helper

---
+++ MarvelApp/Networking/APIHelper.swift
@@ -0,0 +1,143 @@
+//
+//  APIHelper.swift
+//  MarvelApp
+//
+//  Created by Randy Efan Jayaputra on 11/08/22.
+//
+
+import Foundation
+import Alamofire
+import Alamofire_SwiftyJSON
+import SwiftyJSON
+import SwifterSwift
+
+enum ErrorHandlerType {
+    case doNothing
+    case showSnackbarOnly
+    case backToPreviousPage
+    case backToProductHomePage
+    case backToRootPage
+}
+
+protocol ApiInterfaceDelegate: AnyObject {
+    func onSuccess(interface: ApiInterface?, object: Any)
+    func onFailed(interface: ApiInterface?, errorCode: String?, message: String, errorHandlerType: ErrorHandlerType, otherInfo: Any?)
+}
+
+protocol ApiInterface: AnyObject {
+    var method: HTTPMethod? { get set }
+    var url: URLConvertible { get set }
+    var headers: HTTPHeaders? { get set }
+    var parameters: Parameters? { get set }
+    var encoding: ParameterEncoding? { get set }
+    
+    func start()
+    func success(_ value: JSON, _ statusCode: Int)
+    func failed(_ value: JSON?, _ statusCode: Int)
+    func createObject(_ value: Any, _ statusCode: Int)
+}
+
+// MARK: Error Code
+enum ErrorCode: Int {
+    case errNetworkLost = -1005
+    case errHttpLoadFailed = -1003
+    case errBackgroundFetch = 53
+    case errTimeOut = 408
+    case errInternalServer = 500
+    case errUnavailableService = 503
+}
+
+class ApiHelper {
+    
+    static var manager = setupSessionManager()
+    
+    static func setupSessionManager(timeoutForRequest: Double = 300, timeoutForResource: Double = 300) -> SessionManager {
+        let configuration = URLSessionConfiguration.default
+        
+        // Set Timeout
+        configuration.timeoutIntervalForRequest = timeoutForRequest
+        configuration.timeoutIntervalForResource = timeoutForResource
+        
+        return Alamofire.SessionManager(configuration: configuration)
+    }
+    
+    
+    static func apiRequest(api: ApiInterface, isUseDefaultHeader: Bool = true, isUseDefaultParams: Bool = true) {
+        ApiHelper.manager
+            .requestWithoutCache(api.url, method: api.method ?? .get, parameters: [:], encoding: api.encoding ?? URLEncoding.default, headers: api.headers ?? [:])?
+            .validate(statusCode: 200..<600)
+            .responseSwiftyJSON(completionHandler: { (response) in
+                switch response.result {
+                case .success(response.result.value):
+                    // Valid JSON Response
+                    guard let value = response.result.value else {
+                        // No Response Value
+                        print("Default Error Message")
+                        api.failed(nil, response.response?.statusCode ?? -1)
+                        return
+                    }
+                    
+                    // Valid Success Response
+                    api.success(value, response.response?.statusCode ?? -1)
+                case .failure(let error):
+                    if response.response?.statusCode == ErrorCode.errInternalServer.rawValue || response.response?.statusCode == ErrorCode.errUnavailableService.rawValue {
+                        // Internet Lost, Service Unavailable, Internal Server Error
+                        print("Internet Lost, Service Unavailable, Internal Server Error")
+                        api.failed(nil, response.response?.statusCode ?? -1)
+                    } else if error._code == NSURLErrorTimedOut || response.response?.statusCode == ErrorCode.errTimeOut.rawValue {
+                        // RTO
+                        print("Request Time Out")
+                        api.failed(nil, response.response?.statusCode ?? -1)
+                    } else if error._code == NSURLErrorCancelled {
+                        // Cancelled
+                        print("Cancelled")
+                        api.failed(["network_error": NSURLErrorCancelled], response.response?.statusCode ?? -1)
+                    } else if error._code == ErrorCode.errBackgroundFetch.rawValue || error._code == ErrorCode.errNetworkLost.rawValue || error._code == ErrorCode.errHttpLoadFailed.rawValue {
+                        // Failed connection in background
+                        print("Need to retry api call")
+                        api.failed(["network_error": ErrorCode.errBackgroundFetch.rawValue], response.response?.statusCode ?? -1)
+                    } else {
+                        // Valid JSON Response
+                        guard let value = response.result.value else {
+                            // No Response Value
+                            print("Default Error Message")
+                            api.failed(nil, response.response?.statusCode ?? -1)
+                            return
+                        }
+                        
+                        // Valid Failed Response
+                        api.failed(value, response.response?.statusCode ?? -1)
+                    }
+                default:
+                    break
+                }
+            })
+    }
+}
+
+
+extension Alamofire.SessionManager {
+    
+    @discardableResult
+    open func requestWithoutCache(
+        _ url: URLConvertible,
+        method: HTTPMethod = .get,
+        parameters: Parameters? = nil,
+        encoding: ParameterEncoding = URLEncoding.default,
+        headers: HTTPHeaders? = nil) // also you can add URLRequest.CachePolicy here as parameter
+        -> DataRequest? {
+            do {
+                var urlRequest = try URLRequest(url: url, method: method, headers: headers)
+                urlRequest.cachePolicy = .reloadIgnoringCacheData // <<== Cache disabled
+                let encodedURLRequest = try encoding.encode(urlRequest, with: parameters)
+                return request(encodedURLRequest)
+            } catch {
+                // MARK: find a better way to handle error
+                print(error)
+                
+                guard let url = "http://example.com/wrong_request".url else { return nil }
+                return request(URLRequest(url: url))
+            }
+    }
+    
+}
