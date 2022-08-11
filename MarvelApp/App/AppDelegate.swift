d8f64aeb802dc3ea42e70eae110d08443ce6ba42
Randy Efan <randyrandyrej8@gmail.com>
2022-08-11 22:17:55 +0700

    init project

---
+++ MarvelApp/App/AppDelegate.swift
@@ -0,0 +1,33 @@
+//
+//  AppDelegate.swift
+//  MarvelApp
+//
+//  Created by Randy Efan Jayaputra on 11/08/22.
+//
+
+import UIKit
+
+@main
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        self.window = UIWindow(frame: UIScreen.main.bounds)
+        window?.rootViewController = UIStoryboard(name: "Main", bundle: nil).instantiateInitialViewController()
+        window?.makeKeyAndVisible()
+        
+        return true
+    }
+
+    // MARK: UISceneSession Lifecycle
+
+    func application(_ application: UIApplication, configurationForConnecting connectingSceneSession: UISceneSession, options: UIScene.ConnectionOptions) -> UISceneConfiguration {
+        return UISceneConfiguration(name: "Default Configuration", sessionRole: connectingSceneSession.role)
+    }
+
+    func application(_ application: UIApplication, didDiscardSceneSessions sceneSessions: Set<UISceneSession>) {}
+
+
+}
+
---
+++ Podfile
@@ -0,0 +1,27 @@
+# Uncomment the next line to define a global platform for your project
+# platform :ios, '9.0'
+
+target 'MarvelApp' do
+  # Comment the next line if you don't want to use dynamic frameworks
+  use_frameworks!
+
+  # Network Stuff
+  pod 'Alamofire', '4.8.2'
+  pod 'AlamofireObjectMapper', '5.2.0'
+  pod 'AlamofireImage', '3.5.2'
+  pod 'Alamofire-SwiftyJSON', '3.0.0'
+  pod 'Ipify', '2.1.0'
+
+  # Cache Stuff
+  pod 'Kingfisher', '5.5.0'
+  
+  # UI Stuff
+  pod 'IGListKit', '4.0.0'
+
+  # Pods for MarvelApp
+  pod 'SwiftyVIPER', '1.2.3'
+  pod 'RxCocoa', '5.1.1'
+  pod 'RxSwift', '5.1.1'
+  pod 'SwifterSwift', '5.0.0'
+
+end
